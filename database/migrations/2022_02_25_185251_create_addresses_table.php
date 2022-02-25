29be44488d878a1b63b41f5235f3374527efe071
ArtificiallySmart <bryanparcival@gmail.com>
2022-02-25 20:06:58 +0100

    created addresses table

---
+++ database/migrations/2022_02_25_185251_create_addresses_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->unsignedSmallInteger('house_number');
+            $table->string('house_number_addition');
+            $table->string('postal code');
+            $table->string('city');
+            $table->string('country');
+            $table->enum('address_type', ['shipping_address', 'postal_address']);
+            $table->timestamps();
+        });
+
+        Schema::create('phone_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('phone_number');
+            $table->timestamps();
+        });
+
+        Schema::create('user_has_address', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('address_id');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
