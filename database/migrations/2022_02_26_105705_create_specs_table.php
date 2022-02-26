906119eb60a3404008bcde6e664079e740d18f53
ArtificiallySmart <bryanparcival@gmail.com>
2022-02-26 12:21:27 +0100

    created specs table

---
+++ database/migrations/2022_02_26_105705_create_specs_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['colors', 'sizes', 'other']);
+            $table->timestamps();
+        });
+        Schema::create('product_has_specs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('specs_id');
+            $table->foreign('specs_id')->references('id')->on('specs');
+            $table->timestamps();
+        });
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->timestamps();
+        });
+        Schema::create('product_has_category', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('url');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->foreign('deleted_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+        Schema::dropIfExists('product_has_category');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('product_has_specs');
+        Schema::dropIfExists('specs');
+    }
+}
