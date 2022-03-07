840d900fcd283f8adf79668d0c97acd7c86b09be
ZuliEyvazov <zuleyey@gmail.com>
2022-03-07 15:43:05 +0100

    some basic css styling

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -23,6 +23,7 @@
 
 
 
+
              </div>
 
 
@@ -32,28 +33,34 @@
 
              <div class="col-md-6">
                  <div class="row">
-                     <div class="col-md-12">
+                     <div class="col-md-7">
                          <h1 span class="label label-primary">Skeleton</h1>
 
 
 
-                         <button>Add to Cart</button>
 
+                         <h2 span class="p-price">€19,99</h2></span></h2>
 
 
 
                          <p class="description">
+
                              Men's T-shirt. We will print the product for you after you place the order.
                              A t-shirt with an awesome design for a night out or just hanging out with friends!
                          </p>
 
 
+                         <span>Available colors</span>
+                         <div class="colors"> <span></span> <span></span> <span></span> <span></span>
+                         </div>
 
 
+                         <button class="p-button">Add to Cart</button>
 
 
                          <div class="row">
                              <div class="col-md-3">
+
                                  <span class="sr-only">Four out of Five Stars</span>
                                  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
                                  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
@@ -62,6 +69,12 @@
                                  <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
                                  <span class="label label-success">61</span>
                              </div>
+
+
+
+
+
+
                              <div class="col-md-3">
                                  <span class="monospaced">Write a Review</span>
                              </div>
@@ -92,7 +105,9 @@
              </div>
 
 
+
              <footer-component></footer-component>
 
  </body>
+
  @endsection
