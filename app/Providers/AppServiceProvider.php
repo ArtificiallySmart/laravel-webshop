43807255be28ef49c90d7d7ef647e0f3287d51d4
ArtificiallySmart <bryanparcival@gmail.com>
2022-05-10 16:43:28 +0200

    bugfix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,6 +31,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (!Schema::hasTable('orders')) {
+            return;
+        }
         Paginator::useBootstrap();
 
         $this->newOrdersCount = Order::where('status', 'pending')->count();
